60f15d634044e882ffe3c85229d091ee1dd8462a
didiermunezero <didiermunezer38@gmail.com>
2021-06-04 23:39:29 +0200

    feat: remove unneeded

--- src/app/components/report-form/report-form.component.ts
+++ src/app/components/report-form/report-form.component.ts
@@ -1,55 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {ThemePalette} from '@angular/material/core';
 import { MatChipInputEvent } from '@angular/material/chips';
 export interface Fruit {
   name: string;
 }
 
-export interface Task {
-  name: string;
-  completed: boolean;
-  color: ThemePalette;
-  subtasks?: Task[];
-}
 @Component({
   selector: 'app-report-form',
   templateUrl: './report-form.component.html',
   styleUrls: ['./report-form.component.scss']
 })
 export class ReportFormComponent implements OnInit {
-
-  task: Task = {
-    name: 'Indeterminate',
-    completed: false,
-    color: 'primary',
-    subtasks: [
-      {name: 'Eucalyptus', completed: false, color: 'primary'},
-      {name: 'Bamboo', completed: false, color: 'primary'},
-    ]
-  };
-
   allComplete: boolean = false;
 
-  updateAllComplete() {
-    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
-  }
-
-  someComplete(): boolean {
-    if (this.task.subtasks == null) {
-      return false;
-    }
-    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
-  }
-
-  setAll(completed: boolean) {
-    this.allComplete = completed;
-    if (this.task.subtasks == null) {
-      return;
-    }
-    this.task.subtasks.forEach(t => t.completed = completed);
-  }
   visible = true;
   selectable = true;
   removable = true;
@@ -61,12 +25,10 @@ export class ReportFormComponent implements OnInit {
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
-    // Add our fruit
+
     if (value) {
       this.fruits.push({name: value});
     }
-
-    // Clear the input value
     event.chipInput!.clear();
   }
 
