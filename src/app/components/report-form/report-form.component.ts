671c3a64ac8ce3c9b6c0e38de676e03834405c5a
didiermunezero <didiermunezer38@gmail.com>
2021-06-08 23:57:16 +0200

    fix: relook on stepps

--- src/app/components/report-form/report-form.component.ts
+++ src/app/components/report-form/report-form.component.ts
@@ -17,10 +17,6 @@ export interface Data{
   styleUrls: ['./report-form.component.scss']
 })
 export class ReportFormComponent implements OnInit {
-  allComplete: boolean = false;
-
-  feeding: Data[] = [{id: 1,name:"Eucalyptus"},{id: 2,name:"Bamboo"}];
-  // distance: Data[] = ["<50 M","-50-100M",">100M"]
 
   visible = true;
   selectable = true;
@@ -52,42 +48,25 @@ export class ReportFormComponent implements OnInit {
   secondFormGroup!: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) {
-    this.toppings = _formBuilder.group({
-      pepperoni: false,
-      extracheese: false,
-      mushroom: false
+    this.feeding = _formBuilder.group({
+      bamboo: false,
+      eucalyptus: false,
     });
    }
 
-  toppings: FormGroup;
-
-  onCbChange(e:any) {
-    const name: FormArray = this.firstFormGroup.get('name') as FormArray;
-
-    if (e.target.checked) {
-      name.push(new FormControl(e.target.value));
-    } else {
-      let i: number = 0;
-      name.controls.forEach((item) => {
-        if (item.value == e.target.value) {
-          name.removeAt(i);
-          return;
-        }
-        i++;
-      });
-    }
- }
+   feeding: FormGroup;
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      date: ['', Validators.required],
-      starttime: ['', Validators.required],
-      endtime: ['', Validators.required],
+      date: [, Validators.required],
+      starttime: [, Validators.required],
+      endtime: [, Validators.required],
       group: ['', Validators.required],
       none_habits: ['', Validators.required],
       location: ['', Validators.required],
       nIndividuals: [, Validators.required,],
       nOutIndividuals: [, Validators.required,],
-      time: this._formBuilder.array([], [Validators.required])
+      distance: [, Validators.required],
+      village: ['', Validators.required],      
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
