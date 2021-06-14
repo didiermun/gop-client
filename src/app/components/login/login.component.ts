551898ec4f74fb1ee2b3f883ec0bc7cde6f0ddad
didiermunezero <didiermunezer38@gmail.com>
2021-06-14 04:07:24 +0200

    feat: first mutaition

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo,gql } from 'apollo-angular';
+import { Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+
+const LOGIN = gql`
+  mutation login($data: LoginInput!) {
+    login(data: $data) {
+      token
+      success
+    }
+  }
+`;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginFormGroup!: FormGroup;
 
   hide: boolean = false;
 
-  constructor() { }
+  constructor(private apollo: Apollo,private router: Router,private _formBuilder: FormBuilder) { }
+
+  submit(){
+    this.apollo.mutate({
+      mutation: LOGIN,
+      variables: {
+        data: this.loginFormGroup.value
+      }
+    }).subscribe(({ data }) => {
+      let res: any = data;
+      console.log(res);
+      if(res.login.success){
+        // this.notifier.notify('success', 'Login successful');
+        localStorage.setItem("token",res.login.token)
+        this.router.navigateByUrl("/");
+      }
+      else{
+        // this.notifier.notify('error','Code not found');
+      }
+    },(error) => {
+      console.log(error);
+      // this.notifier.notify('error', `${error.message}`);
+    });
+  }
 
   ngOnInit(): void {
+    this.loginFormGroup = this._formBuilder.group({
+      code: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
 }
--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -5,7 +5,7 @@ import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloLink } from 'apollo-link';
 import { setContext } from 'apollo-link-context';
-const uri = 'https://anti-poaching.herokuapp.com/graphql';
+const uri = 'https://gop-backend.herokuapp.com/graphql';
 
 export function provideApollo(httpLink: HttpLink) {
   const basic = setContext((operation, context) => ({
