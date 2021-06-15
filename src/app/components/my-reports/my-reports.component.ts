ac0572eb5c9b5ade6f8e34a017a047a03f0a01b9
didiermunezero <didiermunezer38@gmail.com>
2021-06-15 22:43:15 +0200

    feat: fix left part with footer links

--- src/app/components/my-reports/my-reports.component.ts
+++ src/app/components/my-reports/my-reports.component.ts
@@ -1,5 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, QueryRef,gql } from 'apollo-angular';
+import { Subscription } from 'rxjs';
+import { Router} from '@angular/router'
 
+const GET_REPORTS =  gql`
+  query reports($page: Int){
+    reports(page: $page){ 
+      _id
+      reporter{
+        leader
+      }
+      baseInfo{
+        individuals
+        Oindividuals
+        family
+      }
+      health{
+        sick
+      }
+      interaction{
+        reaction
+      }
+      timing{
+        end
+        start
+        date
+      }
+    }
+  }
+`
 @Component({
   selector: 'app-my-reports',
   templateUrl: './my-reports.component.html',
@@ -7,9 +36,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyReportsComponent implements OnInit {
 
-  constructor() { }
+  reports: any[] = [];
+  reportsQuery!: QueryRef<any>;
+  private querySubscription!: Subscription;
+  loading: boolean = true;
+  page:number =  1;
+
+  constructor(private apollo: Apollo,private router: Router) { }
+
+  loadMore() {
+    this.reportsQuery.refetch({page: this.page})
+  }
 
   ngOnInit(): void {
+    this.reportsQuery = this.apollo.watchQuery<any>({
+      query: GET_REPORTS,
+      pollInterval: 0,
+      variables:{
+        page: this.page
+      }
+    });
+    this.querySubscription = this.reportsQuery
+      .valueChanges
+      .subscribe(({ data, loading }) => {
+        this.loading = loading;
+        this.reports = [...this.reports,...data.reports];
+        this.page++;
+    },(error) => {
+      if(error.graphQLErrors[0].status == 401){
+        localStorage.removeItem('gop_app_token')
+        this.router.navigateByUrl('/login');
+      }
+      console.log('error', `${error.message}`);
+    });
+  }
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
   }
 
 }
