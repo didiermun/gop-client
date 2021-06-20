0c9a764a5645388859dc33a5177a86203d1c127e
didiermunezero <didiermunezer38@gmail.com>
2021-06-20 05:03:29 +0200

    fix: bookmarks with  a single function

--- src/app/components/r-card/r-card.component.ts
+++ src/app/components/r-card/r-card.component.ts
@@ -1,5 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Apollo,gql } from 'apollo-angular';
 
+
+const BOOK_REPORT = gql`
+  mutation bookReport($id: ID!) {
+    bookReport(id: $id) {
+      message
+      success
+    }
+  }
+`;
 @Component({
   selector: 'app-r-card',
   templateUrl: './r-card.component.html',
@@ -8,9 +18,29 @@ import { Component, Input, OnInit } from '@angular/core';
 export class RCardComponent implements OnInit {
   @Input() report: any = {};
 
-  constructor() {
+  constructor(private apollo: Apollo) {
 
-   }
+  }
+
+  submit(){
+    this.apollo.mutate({
+      mutation: BOOK_REPORT,
+      variables: {
+        id: this.report._id
+      }
+    }).subscribe(({ data }) => {
+      let res: any = data;
+      console.log(data);
+    },(error) => {
+      console.log(error.networkError);
+      if(error.networkError){
+        console.log("Slow or no internet detected");
+      }
+      else if(error.graphQLErrors){
+        console.log(error.graphQLErrors[0].message);
+      }
+    });
+  }
   
 
   ngOnInit(): void {
