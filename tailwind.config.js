1e4f6d30b9d975548b8831c72ead88bb5a5fc2c0
Didien MUNEZERO <didiermunezer38@gmail.com>
2021-10-12 18:02:08 +0200

    fix: rewritr sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
     },
     darkMode: 'media', // or 'media' or 'class'
     theme: {
+      fontFamily: {
+        side: 'Roboto',
+      },
       extend: {},
     },
     variants: {
